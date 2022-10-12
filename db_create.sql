736c82dd2f2700fdff23f1393450575b430a6171
Alex <apostlmayr@oakland.edu>
2022-10-12 11:23:26 +0000

    added db_create file

---
+++ db_create.sql
@@ -0,0 +1,21 @@
+CREATE EXTENSION "uuid-ossp";
+
+CREATE TABLE cars(
+    car_id         UUID DEFAULT uuid_generate_v4(),
+    make           text,
+    model          text,
+    year          int,
+    odometer      int,
+    user_id          uuid,
+    last_update     timestamp
+);
+
+CREATE TABLE users (
+    user_id         UUID DEFAULT uuid_generate_v4(),
+    name            text,
+    email           text,
+    phone           text,
+    date     timestamp DEFAULT NOW()
+);
+
+
